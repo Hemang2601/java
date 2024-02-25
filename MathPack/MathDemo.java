41d43835f25661b99728c02f3fa31ec5cf26e2b4
Hemang2601 <144898904+Hemang2601@users.noreply.github.com>
2024-02-25 23:39:05 +0530

    Add files via upload

---
+++ MathPack/MathDemo.java
@@ -0,0 +1,11 @@
+package MathPack;
+
+public class MathDemo {
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int sub(int a, int b) {
+        return a - b;
+    }
+}
